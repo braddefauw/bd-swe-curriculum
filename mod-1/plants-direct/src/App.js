90d7fda1b98f0cb3953b528b7d57954d63505080
braddefauw <badefauw@gmail.com>
2022-02-17 16:54:36 -0600

    fixed add to cart event 1.3.5

--- mod-1/plants-direct/src/App.js
+++ mod-1/plants-direct/src/App.js
@@ -1,20 +1,37 @@
 import Home from './Home'
 import Contact from './Contact'
 import About from './About'
+import { useState } from 'react'; 
+import data from './products.json';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom'
 
 function App() {
+  const [cart, setCart] = useState(data.cart);
+
+  const addToCart = product => {
+      if(cart.indexOf(product) === -1){
+      setCart([...cart, product])
+      product.addedToCart = true;
+      }else{
+      const newCart = cart.filter((oldProduct) => oldProduct !== product);
+      product.addedToCart = false;
+      setCart(newCart);
+      }
+  }
+  
   return (
     <Router>
       <header>
+        <h1>Plants Direct</h1>
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
+          <Link class="header-link" to="/">Home</Link>
+          <Link class="header-link" to="/about">About</Link>
+          <Link class="header-link" to="/contact">Contact</Link>
+          <section class="cart-length">🛒 ({cart.length})</section>
         </nav>
       </header>
       <Routes>
-        <Route path="/" element = {<Home />} />
+        <Route path="/" element = {<Home addToCart={addToCart} data={data}/>} />
         <Route path="/about" element = {<About />} />
         <Route path="/contact" element = {<Contact />} />
       </Routes>
--- mod-1/plants-direct/src/Home.js
+++ mod-1/plants-direct/src/Home.js
@@ -1,28 +1,10 @@
-import { useState } from 'react'; 
 import './App.css';
-import data from './products.json';
 import { Product } from './Product';
 
-function Home(){
-    const [cart, setCart] = useState(data.cart);
-
-    const addToCart = product => {
-        if(cart.indexOf(product) === -1){
-        setCart([...cart, product])
-        product.addedToCart = true;
-        }else{
-        const newCart = cart.filter((oldProduct) => oldProduct !== product);
-        product.addedToCart = false;
-        setCart(newCart);
-        }
-    }
-
-    console.log(cart)
-    
+function Home(props){
     return (
         <div className="App">
-        {data.products.map(product => <Product key={product.productId} product={product} addToCart={addToCart}/>)}
-        <h4>You have {cart.length} items in your cart.</h4>
+        {props.data.products.map(product => <Product key={product.productId} product={product} addToCart={props.addToCart}/>)}
         </div>
     );
 }
