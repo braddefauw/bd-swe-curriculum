281d31ef11aecfd31df664c3515ef668adee75fe
braddefauw <badefauw@gmail.com>
2022-05-05 12:49:03 -0500

    react project intialized + first component

---
+++ mod-1/react/my-app/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  const sauceList = ['Sriracha', 'Tobasco', 'Salsa', 'Sambal', 'Green Sauce'];
+  
+  //1st arg = what is state element called in HTML
+  //2nd arg = what function will update the state?
+  const [sauces, setSauces] = useState(sauceList) //what do we want our default state to be?
+
+  return(
+    <div>
+      <h2>My super React site!</h2>
+      {
+        //insert JS into HTML w/ curly braces
+        //whenever you map over a list, include an index key
+        sauces.map((sauce, idx) => <h3 key={idx}> {sauce}</h3>)
+      }
+    </div>
+  )
+}
+
+export default App;
